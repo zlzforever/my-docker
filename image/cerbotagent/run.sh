a78963f927a4d3f4f00f37d9a3ae1e04e68aa2ff
Lewis Zou <zlzforever@163.com>
2021-08-13 13:34:42 +0800

    添加 https 认证

---
+++ image/cerbotagent/run.sh
@@ -0,0 +1,4 @@
+docker run --rm cerbotagent -e Ali_Key=LTAI5tNmoP9oUPn1DgyX3WgV \
+    -e Ali_Secret=nm9sbXA66RhJ4ipgEFLhTlbtcbDTOr \
+    -e domain=*.hdlinye.com \
+    -v ~/test/pems:~/.acme.sh/*.hdlinye.com/
